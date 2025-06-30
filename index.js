c257b024fcade548fc2b35466bae4dfdd6eba47d
ArvaNyan <rafiarvaputra@gmail.com>
2025-06-30 20:53:10 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.login(token);
